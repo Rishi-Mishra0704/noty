425a149c9dc7bf52e44a50c5dada71d05d2f2971
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-02-18 20:16:47 +0530

    add share note

--- apis/urls.py
+++ apis/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import note,note_create
+from .views import note,note_create,share_note
 urlpatterns = [
     path('notes/<int:note_id>', note, name="note_details"),
     path('notes/create/', note_create, name="note_create"),
+    path('notes/share/', share_note, name="share_note"),
+
 ]
--- apis/views.py
+++ apis/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework import status
+from django.contrib.auth.models import User
 
 
 from .models import Note
@@ -28,7 +29,6 @@ def note(request, note_id):
     elif request.method == 'PUT':
         serializer = NoteSerializer(note, data=request.data)
         if serializer.is_valid():
-            # Set the owner of the note to the authenticated user
             serializer.validated_data['owner'] = request.user
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -51,3 +51,29 @@ def note_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def share_note(request):
+    try:
+        note_id = request.data.get('note_id')
+        shared_to_id = request.data.get('shared_to_id')
+        note = Note.objects.get(pk=note_id)
+        shared_to_user = User.objects.get(pk=shared_to_id)
+    except (Note.DoesNotExist, User.DoesNotExist):
+        return Response({'detail': 'Note or user not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if note.owner != request.user:
+        return Response({'detail': 'You do not have permission to share this note'}, status=status.HTTP_403_FORBIDDEN)
+
+    shared_note_data = {
+        'note': note,
+        'shared_from': request.user,
+        'shared_to': shared_to_user
+    }
+    serializer = SharedNoteSerializer(data=shared_note_data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
